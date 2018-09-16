cd71c92b6d9e4074d5fc6f886812c9ee8179f84c
pallat <yod.pallat@gmail.com>
2018-09-16 09:40:05 +0700

    fix signal example

--- channel/noguarantee.go
+++ channel/noguarantee.go
@@ -9,8 +9,9 @@ func main() {
 	ch := make(chan time.Time, 2)
 	go sender(ch)
 	go sender(ch)
+
 	receiver(ch)
-	fmt.Println("I'm going to throw away the lest")
+	fmt.Println("I'm going to throw away the rest")
 }
 
 func sender(ch chan<- time.Time) {
@@ -21,6 +22,7 @@ func sender(ch chan<- time.Time) {
 
 func receiver(ch <-chan time.Time) {
 	fmt.Println("receiver: waiting for 5 seconds")
+	time.Sleep(time.Second * 5)
 	fmt.Println("receiver: ready")
 	fmt.Println(time.Since(<-ch))
 }
